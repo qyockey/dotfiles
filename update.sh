452daa257de12493a444561af9e329a1da02eebc
Quinn Yockey <qyockey@tutanota.de>
2022-08-06 19:48:39 -0700

    test_tset

--- update.sh
+++ update.sh
@@ -48,7 +48,7 @@ updateFiles () {
 
 pushToGitHub () {
     runCommand "adding all files for commit" "git add -A"
-    runCommand "committing" "git commit -m '$message'"
+    runCommand "committing" "git commit -m $message"
     runCommand "pushing changes" "git push"
 }
 
